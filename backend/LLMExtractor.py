655b00f9bbcdfb9d3d793cc24704607a42b2b2c6
Silversoul-07 <134297914+Silversoul-07@users.noreply.github.com>
2024-03-18 03:31:34 +0530

    Update LLMExtractor.py

--- backend/LLMExtractor.py
+++ backend/LLMExtractor.py
@@ -1,40 +1,13 @@
-from urllib.parse import urlparse
 import trafilatura
-import requests
-from fake_useragent import UserAgent
-from bs4 import BeautifulSoup
-from googlesearch import search
 import textwrap
+from googlesearch import search
+from urllib.parse import urlparse 
 from IPython.display import Markdown
 import google.generativeai as genai
-from gnews import GNews
-
 
 def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
-
-def news_query(query, limit=5):
-    results = []
-    google_news = GNews()
-    news = google_news.get_news(query)
-    for i in range(limit):
-        results.append(news[i]['url'])
-    return results
-
-def extract_heading_from_url(url):
-    r = requests.get(url, headers={'User-Agent': UserAgent().random}
-)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    try:
-      return [heading.text for heading in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])][0]
-    except:
-      return "No heading"
-def extract_text_from_url(url):
-   print(f"Fetching:{url}")
-   downloaded = trafilatura.fetch_url(url)
-   text = trafilatura.extract(downloaded)
-   return  text if text else ""
 
 def is_url(url):
   try:
@@ -43,21 +16,36 @@ def is_url(url):
   except ValueError:
     return False
 
-def predict(my_news):
-    query = my_news
+def news_sources(query, limit=5):
+    results = []
+    for j in search(query, num_results=limit, sleep_interval=2):
+        results.append(j)
+    return results
 
-    if is_url(my_news):
-      query = extract_heading_from_url(my_news) 
-      my_news = extract_text_from_url(my_news)  
-    
-    corpus_of_links = ''
-    for url in news_query(query):
-        corpus_of_links += extract_text_from_url(url) + '\n'
-
-    genai.configure(api_key='AIzaSyAx90CfmOJc3qUC_Y5f7OmQGhrcbYlYjDQ')
+def extract_text_from_url(url):
+   if not is_url(url):
+      return
+   downloaded = trafilatura.fetch_url(url)
+   text = trafilatura.extract(downloaded)
+   if text:
+      return text
+   
+def llm_response(corpus, statement):
+    genai.configure(api_key='AIzaSyAlRtzd89OYpN0VXhdZ8R3w3oWGcIUAFs0')
     model = genai.GenerativeModel('gemini-pro')
     response = model.generate_content(
-       f'''Analyze the provided links: {corpus_of_links}.can you answer whether the following statement with explanation is baised/unbaised or truth/fake: {my_news}'''
+       f'''Analyze the provided text in format url, text from url: {corpus}. from the above text can you answer whether the following statement is baised/unbaised and truth/fake and genre like politics/sports/entertainment/business/education or any field related. Explain your claims with url from above: {statement}'''
     )
     return to_markdown(response.text) 
 
+def main(statement):
+    urls = news_sources(statement)
+    corpus = ''
+    for url in urls:
+        text = extract_text_from_url(url)
+        if text:
+            corpus += f'{url}: {text}\n\n'
+
+    return llm_response(corpus, statement)
+
+main(input("Input: "))
